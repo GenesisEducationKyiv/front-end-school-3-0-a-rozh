a51533b57899dcec5c4853f876ea3e97e2560fb8
Rozhmanov <a.rozhmanov.work@gmail.com>
2025-06-19 09:24:23 +0300

    Add Playwright Component test for ConfirmationModal

---
+++ playwright/index.ts
@@ -0,0 +1,9 @@
+import { beforeMount, afterMount } from '@playwright/experimental-ct-react/hooks';
+
+beforeMount(async () => {
+    // Component test setup
+});
+
+afterMount(async () => {
+    // Component test cleanup
+});
