81b2b13be8906e8f7634ef58728a8eeb497dbf07
Rozhmanov <a.rozhmanov.work@gmail.com>
2025-07-18 12:02:46 +0300

    Fix tests issue with env setup

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
     plugins: [react()],
     test: {
+        globals: true,
         environment: 'jsdom',
-        setupFiles: ['./__tests__/setup.ts'],
+        setupFiles: path.resolve(__dirname, '__tests__', 'setup.ts'),
+        env: {
+            VITE_API_BASE_URL: 'http://localhost:8000/api',
+        },
         exclude: [
             'node_modules',
             'dist',
